571f8d03ad7c1e311f38019ae5a007d7b1b44000
monwisn <48805011+monwisn@users.noreply.github.com>
2022-05-19 18:44:12 +0200

    Update file.py

--- file.py
+++ file.py
@@ -4,9 +4,29 @@ def greeter(func):
 
     return wrapper
 
-
+ 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def wrapper(*args):
+        numbers = func(*args).split(' ')
+        list_of_nums = []
+
+        for number in numbers:
+            if number[0] == '-':
+                nums = [-int(n) for n in number[1:]]
+            else:
+                nums = [int(n) for n in number[:]]
+
+            list_of_nums.append(nums)
+
+        sum1 = sum(list_of_nums[0])
+        sum2 = sum(list_of_nums[1])
+
+        if sum1 == sum2:
+            print(f'{sum1} == {sum2}')
+        else:
+            print(f'{sum1} != {sum2}')
+
+    return wrapper()
 
 
 def format_output(*required_keys):
